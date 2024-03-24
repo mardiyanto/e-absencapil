b997907a9f9e626e2665b67186875451b5aed59f
mardiyanto <mardybest@gmail.com>
2024-03-24 14:54:45 +0700

    fix

--- utama.php
+++ utama.php
@@ -116,26 +116,7 @@ $total = $total_presensi -$total_presensi_datang;
                     <!-- Content Row -->
   <div class="row">
 
-<div class="col-xl-12 col-lg-12">
 
-    <!-- Area Chart -->
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
-        </div>
-        <div class="card-body">
-            <div class="chart-area">
-                <canvas id="myAreaChart"></canvas>
-            </div>
-            <hr>
-            Styling for the area chart can be found in the
-            <code>/js/demo/chart-area-demo.js</code> file.
-        </div>
-    </div>
-
-    
-
-</div>
 
 <!-- Donut Chart -->
 <div class="col-xl-12 col-lg-12">
